c79b85d1e0649ebdd598d0787c639416a4627070
Kurichi <yuuyapp080@gmail.com>
2023-12-12 08:09:41 +0900

    頑張った

--- terraform/modules/vpc.tf
+++ terraform/modules/vpc.tf
@@ -20,3 +20,22 @@ resource "google_compute_subnetwork" "private" {
   ip_cidr_range = var.vpc.private_subnet_cidr
   network       = google_compute_network.vpc.id
 }
+
+resource "google_compute_global_address" "static" {
+  project = var.common.project
+  name    = "${var.common.prefix}-static-ip-${var.common.env}"
+}
+
+resource "google_dns_managed_zone" "dns" {
+  name        = "${var.common.prefix}-dns-${var.common.env}"
+  dns_name    = "${var.common.domain}."
+  description = "Managed by Terraform"
+}
+
+resource "google_dns_record_set" "dns" {
+  name         = "${var.common.domain}."
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.dns.name
+  rrdatas      = [google_compute_global_address.static.address]
+}
