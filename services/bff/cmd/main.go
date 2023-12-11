999a2e24a9c1bffc7d61268c835e31b72296d9e4
Kurichi <yuuyapp080@gmail.com>
2023-12-12 06:15:06 +0900

    fix(bff): health check path

--- services/bff/cmd/main.go
+++ services/bff/cmd/main.go
@@ -9,6 +9,9 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "OK!")
+	})
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(200, "OK!")
 	})
