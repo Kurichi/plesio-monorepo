42ccda9d11db8405be470395a75994e1e72fb2cc
Kurichi <yuuyapp080@gmail.com>
2023-12-12 21:11:22 +0900

    feat(tree): subscribe act-to-tree

---
+++ services/tree/adapter/pubsub/subscriber.go
@@ -0,0 +1,45 @@
+package subscriber
+
+import (
+	"context"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/Kurichi/plesio-monorepo/services/tree/pkg/config"
+	"github.com/pkg/errors"
+)
+
+type Subscriber struct {
+	client *pubsub.Client
+	topic  *pubsub.Topic
+}
+
+func NewSubscriber(ctx context.Context) (*Subscriber, error) {
+	cfg := config.NewPubSubConfig()
+	client, err := pubsub.NewClient(ctx, cfg.ProjectID)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return &Subscriber{
+		client: client,
+		topic:  client.Topic(cfg.TopicName),
+	}, nil
+}
+
+func (s *Subscriber) Subscribe(ctx context.Context) error {
+	sub, err := s.client.CreateSubscription(ctx, s.topic.ID(), pubsub.SubscriptionConfig{
+		Topic: s.topic,
+	})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
+		log.Println(string(m.Data))
+	})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
--- services/tree/cmd/main.go
+++ services/tree/cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
+	subscriber "github.com/Kurichi/plesio-monorepo/services/tree/adapter/pubsub"
 	"github.com/Kurichi/plesio-monorepo/services/tree/pkg/config"
 	"github.com/Kurichi/plesio-monorepo/services/tree/register"
 )
@@ -18,6 +20,11 @@ func main() {
 	}
 
 	s := register.New()
+	sub, err := subscriber.NewSubscriber(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	sub.Subscribe(context.Background())
 
 	go func() {
 		log.Printf("start gRPC server! port: %v", cfg.Port)
