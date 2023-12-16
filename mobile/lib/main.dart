ec3b0e007410e8ab8147114a7d8baf421b2e3f28
chnotchy <kokiuaxdx@gmail.com>
2023-12-17 08:35:06 +0900

    Add Kiikuten theme and color classes

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:kiikuten/firebase_options.dart';
+import 'package:kiikuten/presentation/designsystem/style/theme.dart';
 import 'package:kiikuten/presentation/page/auth/auth.dart';
 
 Future<void> main() async {
@@ -29,17 +28,7 @@ class KiikutenApp extends StatelessWidget {
     return MaterialApp(
       title: '木育展',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF166718),
-        ),
-        textTheme: kIsWeb
-            ? GoogleFonts.dotGothic16TextTheme(Theme.of(context).textTheme)
-            : null,
-        // fontFamilyFallback:
-        //     kIsWeb ? const ['${FontFamily.notoSansJP} Regular'] : null,
-      ),
+      theme: KiikutenTheme.theme(context),
       home: const KiikutenAuth(),
     );
   }
