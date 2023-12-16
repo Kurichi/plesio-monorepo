e28f5b2326f4df081a32125bab67d75de52fe9b6
Kurichi <yuuyapp080@gmail.com>
2023-12-16 17:32:59 +0900

    chore(tree): db migration

---
+++ services/item/migration/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/Kurichi/plesio-monorepo/services/item/infra"
+	"github.com/Kurichi/plesio-monorepo/services/item/pkg/config"
+	"github.com/Kurichi/plesio-monorepo/services/item/pkg/database"
+)
+
+func main() {
+	cfg := config.NewDBConfig()
+	db := database.New(cfg)
+	if err := infra.Migrate(db); err != nil {
+		panic(err)
+	}
+
+}
