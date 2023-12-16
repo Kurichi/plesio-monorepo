793a60b857277166cd4a43a4054f1ae6c4a8acb8
chnotchy <kokiuaxdx@gmail.com>
2023-12-16 10:58:55 +0900

    Add description field to Item class

--- mobile/lib/domain/entity/item.dart
+++ mobile/lib/domain/entity/item.dart
@@ -1,12 +1,14 @@
 class Item {
   final String id;
   final String name;
+  final String description;
   final int growthEffect;
   bool isUsed = false;
 
   Item({
     required this.id,
     required this.name,
+    required this.description,
     required this.growthEffect,
   });
 
