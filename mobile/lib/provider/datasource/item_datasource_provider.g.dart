3586d97ad99a1a72929f098f0630cf94f0b8971f
chnotchy <kokiuaxdx@gmail.com>
2023-12-16 18:07:20 +0900

    Add data source providers for item, mission, tree, and user

---
+++ mobile/lib/provider/datasource/item_datasource_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item_datasource_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$itemDataSourceHash() => r'9e3607b4cf62c69b5c10a7d74aefd088a9ee491e';
+
+/// See also [itemDataSource].
+@ProviderFor(itemDataSource)
+final itemDataSourceProvider = AutoDisposeProvider<ItemDataSource>.internal(
+  itemDataSource,
+  name: r'itemDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$itemDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ItemDataSourceRef = AutoDisposeProviderRef<ItemDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ mobile/lib/provider/datasource/mission_datasource_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'mission_datasource_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$missionDataSourceHash() => r'9f3d71ee708d73ad554fe63fbf8c9520b541bcf0';
+
+/// See also [missionDataSource].
+@ProviderFor(missionDataSource)
+final missionDataSourceProvider =
+    AutoDisposeProvider<MissionDataSource>.internal(
+  missionDataSource,
+  name: r'missionDataSourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$missionDataSourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MissionDataSourceRef = AutoDisposeProviderRef<MissionDataSource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
