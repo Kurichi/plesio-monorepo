04a8d6ac751acf559f91b530d75bfa65203b139a
chnotchy <kokiuaxdx@gmail.com>
2023-12-16 12:10:57 +0900

    Add UseItemUseCase class

---
+++ mobile/lib/domain/usecase/use_item_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:kiikuten/domain/repository/item_repository.dart';
+import 'package:kiikuten/domain/repository/tree_repository.dart';
+import 'package:kiikuten/domain/repository/user_repository.dart';
+
+class UseItemUseCase {
+  final UserRepository userRepository;
+  final ItemRepository itemRepository;
+  final TreeRepository treeRepository;
+
+  UseItemUseCase({
+    required this.userRepository,
+    required this.itemRepository,
+    required this.treeRepository,
+  });
+
+  Future<void> execute(String userId, String itemId) async {
+    final user = await userRepository.getUser(userId);
+    final item = await itemRepository.getItem(itemId);
+    await treeRepository.growTree(user.tree, item.growthEffect);
+  }
+}
