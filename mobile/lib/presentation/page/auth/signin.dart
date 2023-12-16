5d5180e7f706022c4265c1fab269ce818daaa863
chnotchy <kokiuaxdx@gmail.com>
2023-12-17 08:16:53 +0900

    Add font_awesome_flutter package for GitHub sign-in

--- mobile/lib/presentation/page/auth/signin.dart
+++ mobile/lib/presentation/page/auth/signin.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SigninScreen extends StatelessWidget {
   const SigninScreen({super.key});
@@ -21,58 +22,81 @@ class SigninScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              RichText(
-                text: TextSpan(
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                        letterSpacing: 2.0,
-                      ),
-                  children: [
-                    const TextSpan(text: '自分の'),
-                    TextSpan(
-                      text: '木',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 36,
-                      ),
-                    ),
-                    const TextSpan(text: 'を'),
-                    TextSpan(
-                      text: '育',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 36,
-                      ),
-                    ),
-                    const TextSpan(text: 'てる'),
-                    TextSpan(
-                      text: '展',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 36,
-                      ),
-                    ),
-                    const TextSpan(text: '覧会'),
-                  ],
-                ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: FittedBox(
+              child: Icon(
+                Icons.park,
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.08),
               ),
-              const SizedBox(height: 40),
-              FilledButton(
-                onPressed: () async {
-                  await signInWithGitHub();
-                },
-                child: const Text('Sign in with GitHub'),
-              ),
-            ],
+            ),
           ),
-        ),
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style:
+                          Theme.of(context).textTheme.headlineSmall?.copyWith(
+                                letterSpacing: 2.0,
+                              ),
+                      children: [
+                        const TextSpan(text: '自分の'),
+                        TextSpan(
+                          text: '木',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 36,
+                          ),
+                        ),
+                        const TextSpan(text: 'を'),
+                        TextSpan(
+                          text: '育',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 36,
+                          ),
+                        ),
+                        const TextSpan(text: 'てる'),
+                        TextSpan(
+                          text: '展',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 36,
+                          ),
+                        ),
+                        const TextSpan(text: '覧会'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  FilledButton(
+                    onPressed: () async {
+                      await signInWithGitHub();
+                    },
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(FontAwesomeIcons.github),
+                        SizedBox(width: 16, height: 48),
+                        Text('Sign in with GitHub'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
