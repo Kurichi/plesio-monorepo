5ad29932511209ab0834de9d55d747222c3f2d4f
chnotchy <kokiuaxdx@gmail.com>
2023-12-17 10:21:24 +0900

    Update dotCount in KiikutenSeed

--- mobile/lib/presentation/designsystem/component/tree/kiikuten_seed.dart
+++ mobile/lib/presentation/designsystem/component/tree/kiikuten_seed.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:kiikuten/presentation/designsystem/component/dot/brown_dot.dart';
 
@@ -5,7 +7,7 @@ class KiikutenSeed extends StatelessWidget {
   const KiikutenSeed({
     super.key,
     required this.size,
-    this.dotCount = 10,
+    this.dotCount = 16,
   });
 
   final double size;
@@ -17,12 +19,24 @@ class KiikutenSeed extends StatelessWidget {
       width: size,
       height: size,
       child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: dotCount,
         ),
         itemCount: dotCount * dotCount,
         itemBuilder: (context, index) {
-          return const BrownDot();
+          // 中心からの距離を計算
+          final x = index % dotCount;
+          final y = index ~/ dotCount;
+          final center = (dotCount - 1) / 2;
+          final distance = sqrt(pow(x - center, 2) + pow(y - center, 2));
+
+          // 中心からの距離が一定値以下の場合のみドットを表示
+          if (distance < center) {
+            return const BrownDot();
+          } else {
+            return Container(); // 何も描画しない
+          }
         },
       ),
     );
